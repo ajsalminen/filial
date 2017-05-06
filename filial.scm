2cf0c5b50ca1198030b2c7da24503f9c8a20aeaf
Antti Salminen <antti.salminen@gmail.com>
2017-05-06 15:18:42 +0300

    Beginnings of mailcap support.

--- filial.scm
+++ filial.scm
@@ -6,32 +6,63 @@
 (define mailcap_path "/etc/mailcap")
 
 ;; Only process lines that contain mime definitions.
-(define (process-line toplevel line)
+(define (process-line toplevel line matching-definition? process-definition)
   (let ((lineparts (string-split line)))
-    (if (null? lineparts)
-        lineparts
-        (process-mime-definition toplevel lineparts))))
+    (if (< (length lineparts) 2)
+        '()
+        ((process-definition toplevel matching-definition?) lineparts))))
 
 ;; Check if the mimetype's top-level type is the one passed.
-(define (check-mimetype toptype mimetype)
-  (string= toptype (car (string-split mimetype "/"))
-           ))
+(define (check-top-mimetype toptype)
+  (lambda (mimetype)
+    (string= toptype (car (string-split mimetype "/"))
+             )))
+
+;; Check if the mimetype's type is the one passed.
+(define (check-mimetype wanted-mimetype)
+  (lambda (mimetype)
+    (string= wanted-mimetype mimetype)
+    ))
+
+;; Check if the mimetype's type is the one of the passed.
+(define (check-mimetype-list wanted-mimetype)
+  (lambda (mimetype)
+    (member mimetype wanted-mimetype)
+    ))
+
 
 ;; Return extensions for all matching mimetypes.
-(define (process-mime-definition toptype lineparts)
-  (if (check-mimetype toptype (car lineparts))
-      (cdr lineparts)
-      '()
-      ))
+(define (process-mime-definition toptype matching-definition?)
+  (lambda (lineparts)
+    (if ((matching-definition? toptype) (car lineparts))
+        (cdr lineparts)
+        '()
+        )))
+
+;; Return extensions for all matching mimetypes.
+(define (process-mailcap-definition program matching-definition?)
+  (lambda (lineparts)
+    (if ((matching-definition? program) (car(cdr lineparts)))
+        (string-drop-right (car lineparts) 1)
+        '()
+        )))
+
 
 ;; Read file and create a single list of all extensions for the top level type.
-(define (extensions toptype)
+(define (process-config-file path toptype matching-definition? process-definition)
   (flatten
    (map
     (lambda (mimetype)
       (process-line
-       toptype mimetype))
-    (read-lines mime_path))))
+       toptype mimetype matching-definition? process-definition))
+    (read-lines path))))
+
+(define (extensions toptype)
+  (let ((mailcap-result (process-config-file mailcap_path toptype check-mimetype process-mailcap-definition)))
+    ( if (null? mailcap-result)
+         (process-config-file mime_path toptype check-top-mimetype process-mime-definition)
+         (process-config-file mime_path mailcap-result check-mimetype-list process-mime-definition)
+         )))
 
 ;; Format the arguments for use with "find".
 (define (generate-find-args extensions)
